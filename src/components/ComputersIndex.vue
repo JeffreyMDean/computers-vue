9a5a65e956158d26d52cd9b6cde4fdd4727cda12
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-25 15:28:10 -0400

    Started vue setup and created computers index action

---
+++ src/components/ComputersIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    computers: Array,
+  },
+};
+
+</script>
+
+<template>
+  <div>
+    <h1>All computers</h1>
+    <div v-for="computer in computers" v-bind:key="computer.id">
+      <p>{{ computer.make }}</p>
+      <p>{{ computer.model }}</p>
+      <p>{{ computer.year }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/Content.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+import ComputersIndex from "./ComputersIndex.vue";
+
+export default {
+  components: {
+    ComputersIndex,
+  },
+  data: function () {
+    return {
+      computers: [
+        { id: 30, make: "apple", model: "macbook", year: 2015},
+        { id: 31, make: "Dell", model: "inspiron", year: 2017}
+      ],
+    };
+  },
+  created: function () {
+    this.handleIndexComputers();
+  },
+  methods: {
+    handleIndexComputers: function () {
+      axios.get("http://localhost:3000/computers.json").then((response) => {
+        console.log("computers index", response);
+        this.computers = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <ComputersIndex v-bind:computers="computers" />
+  </main>
+</template>
+
+<style></style>
